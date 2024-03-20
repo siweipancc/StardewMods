140d93d47c54b942c1543ab4eb8f58fe43c853b7
siweipancc <siweipancc@hotmail.com>
2024-03-20 14:29:19 +0800

    feat(AutoFillPetBowl): init

---
+++ AutoFillPetBowl/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace AutoFillPetBowl
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += FillBowl;
+        }
+
+        private static void FillBowl(object? sender, DayStartedEventArgs e)
+        {
+            var pet = Game1.MasterPlayer.getPet();
+            pet?.GetPetBowl()?.watered.Set(true);
+        }
+    }
+}
---
+++ AutoFillPetBowl/docs/README.md
@@ -0,0 +1,11 @@
+﻿## Description
+At the start of everyday, it fills your pet's bowl.
+
+## Install
+1. [Install the latest version of SMAPI](https://smapi.io/).
+2. Download this mod and unzip it into Stardew Valley/Mods.
+3. Run the game using SMAPI.
+
+## Compatibility
+- Stardew Valley 1.6.1
+- **SMAPI 4.0**
